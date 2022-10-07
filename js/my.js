765d3014a54e0eaf1ea1d5146526cf54cd3816af
AlexandrKomisar <monolitalex@gmail.com>
2022-10-07 16:53:41 +0300

    func load

--- js/my.js
+++ js/my.js
@@ -4,14 +4,8 @@
 let inp = document.getElementById("inp");
 let btn = document.querySelector(".btn");
 let arr = [];
+let priceWater = 33;
 let arrWater = [10, 11, 12, 13, 3, 6, 7, 8, 11, 4, 5, 7];
-
-// btn.addEventListener("click", function () {
-//   let valInp = inp.value;
-//   arr.push(valInp);
-//   console.log(arr);
-//   inp.value = "";
-// });
 
 btn.addEventListener("click", function () {
   console.log("click");
@@ -23,8 +17,16 @@ btn.addEventListener("click", function () {
 console.log(arr);
 console.log(arrWater);
 
+let sum = 0;
 function loadWater() {
-  console.log(arrWater);
+  let ol = document.createElement("ol");
+
+  for (let i = 0; i < arrWater.length; i++) {
+    sum = sum + arrWater[i] * priceWater;
+    console.log(sum);
+    document.querySelector(".water__load").innerHTML = arrWater[i] * priceWater;
+    document.querySelector(".water__sum").innerHTML = sum;
+  }
 }
 
 document.querySelector(".water__btn").addEventListener("click", loadWater);
